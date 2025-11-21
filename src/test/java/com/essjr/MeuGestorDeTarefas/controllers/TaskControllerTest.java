dd102fd4c5c8e122f06ad953c7eadd0ee7e9eda3
Evandro Sacramento II <evandro-dev@outlook.com>
2025-11-21 20:23:30 -0300

    atualização de testes

--- src/test/java/com/essjr/MeuGestorDeTarefas/controllers/TaskControllerTest.java
+++ src/test/java/com/essjr/MeuGestorDeTarefas/controllers/TaskControllerTest.java
@@ -15,11 +15,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
@@ -33,6 +35,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(TaskController.class) // 1. Diz ao Spring para testar APENAS o TaskController
 @Import(SecurityConfig.class) // Garante que as nossas regras de segurança são carregadas
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TaskControllerTest {
 
     @Autowired
--- src/test/java/com/essjr/MeuGestorDeTarefas/repositories/TaskRepositoryTest.java
+++ src/test/java/com/essjr/MeuGestorDeTarefas/repositories/TaskRepositoryTest.java
@@ -8,8 +8,10 @@ import com.essjr.MeuGestorDeTarefas.models.enuns.UserRole;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -18,6 +20,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class TaskRepositoryTest {
 
     @Autowired
